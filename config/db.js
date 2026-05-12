d1db5881b613c22a26ffa4cd40ef9ff0b0470a65
sandipsawant10 <sandipsawant503@gmail.com>
2026-05-12 08:31:12 +0530

    refactor: remove default database connection values for improved configuration

--- config/db.js
+++ config/db.js
@@ -4,13 +4,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "school_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME ,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+
+  connectTimeout: 10000,
+
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
 pool.getConnection((err, connection) => {
